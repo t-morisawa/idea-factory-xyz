752868b1fcd7928d8b868e5d16c209eca8af9c37
Toma Morisawa <13.35.104@gmail.com>
2019-03-29 23:58:28 +0900

    add description about scamper

---
+++ gatsby/idea-factory/src/pages/scamper.js
@@ -0,0 +1,27 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+export default () => {
+  return (
+      <div class="container">
+      <h1>SCAMPER</h1>
+      <div class="row">
+      <div class="col-lg">
+      <a href="../index.html">
+      <i class="fas fa-arrow-left"></i>
+      </a>
+      </div>
+      <div class="col-lg-8">
+      SCAMPERは新しいアイデアを出すための手法の一種です。
+    既存の製品やサービスを7つの観点から見て考えることによって、 新しい製品やサービスのアイデアを生成するのに役立ちます。
+    </div>
+      <div class="col-lg">
+      <a href="https://t-morisawa.github.io/scamper-ja/">
+      <i class="fas fa-arrow-right"></i>
+      </a>
+      </div>
+      </div>
+      </div>
+  )
+}
