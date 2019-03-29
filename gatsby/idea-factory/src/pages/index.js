480eecc42e7a62328414a4041aa25fd416c7cf32
Toma Morisawa <13.35.104@gmail.com>
2019-03-29 23:50:24 +0900

    update index.js
    add bootstrap modules

--- gatsby/idea-factory/src/pages/index.js
+++ gatsby/idea-factory/src/pages/index.js
@@ -1,3 +1,45 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.css';
 
-export default () => <div>Hello world!</div>
+
+export default () => {
+  return (
+      <div className="container">
+          <h1>アイデアファクトリー</h1>
+          <div className="row">
+              <div className="col-lg"></div>
+              <div className="col-lg-8">
+              <div className="d-flex flex-row justify-content-between">
+                  <div className="p-3">自分に何ができるかを考える</div>
+                  <div className="p-3">SWOT</div>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
+                  <div className="p-3">アイデアを出す</div>
+                  <div className="p-3">ブレインストーミング</div>
+              </div>
+              <a href="./description/scamper.html">
+              <div className="d-flex flex-row justify-content-between">
+                  <div className="p-3">作ったアイデアにひねりを加える</div>
+                  <div className="p-3">SCAMPER</div>
+              </div>
+              </a>
+              <a href="./description/triz.html">
+                  <div className="d-flex flex-row justify-content-between">
+                      <div className="p-3">5分で発明する</div>
+                      <div className="p-3">TRIZ</div>
+                  </div>
+              </a>
+              <div className="d-flex flex-row justify-content-between">
+                  <div className="p-3">作ったアイデアを普及させる</div>
+                  <div className="p-3">AIDMA</div>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
+                  <div className="p-3">マーケティングにオススメの書籍</div>
+                  <div className="p-3"></div>
+              </div>
+          </div>
+          <div className="col-lg"></div>
+          </div>
+      </div>
+  )
+}
