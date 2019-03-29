c303fec361a84e33345c71ca5aec35b68db310b8
Toma Morisawa <13.35.104@gmail.com>
2019-03-30 00:10:37 +0900

    update to use fontawesome icons

--- gatsby/idea-factory/src/pages/scamper.js
+++ gatsby/idea-factory/src/pages/scamper.js
@@ -1,24 +1,26 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
 export default () => {
   return (
-      <div class="container">
+      <div className="container">
       <h1>SCAMPER</h1>
-      <div class="row">
-      <div class="col-lg">
+      <div className="row">
+      <div className="col-lg">
       <a href="../index.html">
-      <i class="fas fa-arrow-left"></i>
+       <FontAwesomeIcon icon={faArrowLeft} />
       </a>
       </div>
-      <div class="col-lg-8">
+      <div className="col-lg-8">
       SCAMPERは新しいアイデアを出すための手法の一種です。
     既存の製品やサービスを7つの観点から見て考えることによって、 新しい製品やサービスのアイデアを生成するのに役立ちます。
     </div>
-      <div class="col-lg">
+      <div className="col-lg">
       <a href="https://t-morisawa.github.io/scamper-ja/">
-      <i class="fas fa-arrow-right"></i>
+      <FontAwesomeIcon icon={faArrowRight} />
       </a>
       </div>
       </div>
