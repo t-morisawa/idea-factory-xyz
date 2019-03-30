dbda7f2c8f662cab28dff41edc0d502388cab507
Toma Morisawa <13.35.104@gmail.com>
2019-03-30 09:49:33 +0900

    update scamper templates

--- gatsby/idea-factory/src/pages/scamper.js
+++ gatsby/idea-factory/src/pages/scamper.js
@@ -1,29 +1,13 @@
-import React from "react"
-import 'bootstrap/dist/css/bootstrap.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import Container from "../components/description";
 
 
 export default () => {
   return (
-      <div className="container">
-      <h1>SCAMPER</h1>
-      <div className="row">
-      <div className="col-lg">
-      <a href="../index.html">
-       <FontAwesomeIcon icon={faArrowLeft} />
-      </a>
-      </div>
-      <div className="col-lg-8">
-      SCAMPERは新しいアイデアを出すための手法の一種です。
-    既存の製品やサービスを7つの観点から見て考えることによって、 新しい製品やサービスのアイデアを生成するのに役立ちます。
-    </div>
-      <div className="col-lg">
-      <a href="https://t-morisawa.github.io/scamper-ja/">
-      <FontAwesomeIcon icon={faArrowRight} />
-      </a>
-      </div>
-      </div>
-      </div>
+      <Container title="SCAMPER"
+    description="SCAMPERは新しいアイデアを出すための手法の一種です。
+    既存の製品やサービスを7つの観点から見て考えることによって、 新しい製品やサービスのアイデアを生成するのに役立ちます。"
+    to="https://t-morisawa.github.io/scamper-ja/"
+      />
   )
 }
