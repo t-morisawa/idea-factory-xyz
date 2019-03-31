368aa82482e4e74ed2fa46b7654a37d44fa9f890
Toma Morisawa <13.35.104@gmail.com>
2019-03-31 10:54:05 +0900

    fix lint error

--- gatsby/idea-factory/src/components/arrow_right.js
+++ gatsby/idea-factory/src/components/arrow_right.js
@@ -1,5 +1,4 @@
 import React from "react"
-import 'bootstrap/dist/css/bootstrap.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "gatsby";
