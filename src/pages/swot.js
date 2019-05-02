7d6886fee3d040c40398b053d96961c377ba5970
Toma Morisawa <13.35.104@gmail.com>
2019-05-02 14:29:10 +0900

    add swot pages

---
+++ src/pages/swot.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../components/description";
+
+
+export default () => {
+  return (
+      <Container title="SWOT"
+    description="SWOT分析（-ぶんせき、SWOT analysis）とは、目標を達成するために意思決定を必要としている組織や個人のプロジェクトやベンチャービジネスなどにおいて、外部環境や内部環境を強み (Strengths)、弱み (Weaknesses)、機会 (Opportunities)、脅威 (Threats) の4つのカテゴリーで要因分析し、事業環境変化に対応した経営資源の最適活用を図る"
+    to="/idea/swot"
+      />
+  )
+}
