d56d7ba436545d322da6c1de574eff7258f1e5db
Toma Morisawa <13.35.104@gmail.com>
2019-05-02 15:55:40 +0900

    fix

--- src/pages/idea/scamper.js
+++ src/pages/idea/scamper.js
@@ -2,12 +2,10 @@ import React from "react";
 import ScriptTag from "../../components/scripttag";
 
 export default () => {
-  const id = Math.random().toString();
-
   return (
     <div>
       <div id="root"></div>
-      <ScriptTag src="https://epic-mayer-262a99.netlify.com/js/idea-raw/scamper.js" id={id} />
+      <ScriptTag src="https://epic-mayer-262a99.netlify.com/js/idea-raw/scamper.js" id="external-script" />
     </div>
    )
 }
