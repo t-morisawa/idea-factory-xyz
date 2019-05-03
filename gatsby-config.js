cfaf8475c5fec0610686de45542be7fb6d95f5d9
Toma Morisawa <13.35.104@gmail.com>
2019-05-03 13:12:00 +0900

    update to use markdown remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sass`,
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown`,
+        name: "markdown-pages",
+      },
+    },
+    `gatsby-transformer-remark`,
   ]
 }
