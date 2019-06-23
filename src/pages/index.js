7b82b8fbc32dc33aa5986e2731548a8d238b4104
Toma Morisawa <13.35.104@gmail.com>
2019-06-23 15:25:36 +0900

    update index to categorize

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,34 +15,47 @@ export default () => {
       <div className={styles.row}>
         <div className={styles.colLg}></div>
         <div className={styles.colLg8}>
+
+      <h2>文章を書く</h2>
+
       <Link to="/desc/saleswriting/">
       <div className={`${styles.dFlex} ${styles.flexRow} ${styles.justifyContentBetween}`}>
       <div className={styles.p3}>顧客を購買行動へと誘導する</div>
       <div className={styles.p3}>セールスコピーライティング</div>
       </div>
       </Link>
+
+      <Link to="/desc/blog/">
+      <div className={`${styles.dFlex} ${styles.flexRow} ${styles.justifyContentBetween}`}>
+      <div className={styles.p3}>読まれるブログ記事を書く</div>
+      <div className={styles.p3}>BLOG</div>
+      </div>
+      </Link>
+
+      <h2>自己分析する</h2>
+
       <Link to="/desc/swot/">
           <div className={`${styles.dFlex} ${styles.flexRow} ${styles.justifyContentBetween}`}>
             <div className={styles.p3}>自分に何ができるかを考える</div>
             <div className={styles.p3}>SWOT</div>
           </div>
           </Link>
+
+      <h2>ベストセラーで学ぶ</h2>
+
       <Link to="/desc/factfulness/">
       <div className={`${styles.dFlex} ${styles.flexRow} ${styles.justifyContentBetween}`}>
       <div className={styles.p3}>データのカラクリを見抜く</div>
       <div className={styles.p3}>ファクトフルネス</div>
       </div>
       </Link>
+
+      <h2>アイデアを生み出す</h2>
+
       <Link to="/desc/scamper/">
       <div className={`${styles.dFlex} ${styles.flexRow} ${styles.justifyContentBetween}`}>
       <div className={styles.p3}>作ったアイデアにひねりを加える</div>
       <div className={styles.p3}>SCAMPER</div>
-      </div>
-      </Link>
-      <Link to="/desc/blog/">
-      <div className={`${styles.dFlex} ${styles.flexRow} ${styles.justifyContentBetween}`}>
-      <div className={styles.p3}>読まれるブログ記事を書く</div>
-      <div className={styles.p3}>BLOG</div>
       </div>
       </Link>
       <Link to="/desc/triz/">
@@ -59,6 +72,7 @@ export default () => {
             <div className={styles.p3}>作ったアイデアを普及させる</div>
             <div className={styles.p3}>AIDMA</div>
             </div> */}
+      <h2>勉強する</h2>
       <div className={`${styles.dFlex} ${styles.flexRow} ${styles.justifyContentBetween}`}>
       <Link to="/books">
       <div className={styles.p3}>最低限読んでおくべきオススメの書籍</div>
